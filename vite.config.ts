a5808241fe766a47a9cd7efd9b77c5c1859e8f22
pixelweb  studio <rizwanmadni517@gmail.com>
2026-04-27 06:00:42 -0700

    added base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Is line ko add karein, ye batayega ke files 'saylanitoken' folder mein hain
+  base: '/saylanitoken/', 
 })
